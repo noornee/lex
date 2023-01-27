9a1e20cf76d9d98393af9ad1b632fa82da99e97c
cmd777 <83428931+cmd777@users.noreply.github.com>
2023-01-27 21:11:40 +0100

    Version checking (not an auto update)

---
+++ src/logic/version/version.go
@@ -0,0 +1,36 @@
+package version
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const (
+	LEX_VERSION = 299734
+)
+
+func CheckForUpdates() (ok bool, latest int) {
+	resp, err := http.Get("https://raw.githubusercontent.com/cmd777/lex/main/VERSION.txt")
+	if err != nil {
+		log.Println(err)
+		return false, 0
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return false, 0
+	}
+
+	version, err := strconv.Atoi(string(body))
+	if err != nil {
+		log.Println(err)
+		return false, 0
+	}
+
+	return true, version
+}
--- src/main.go
+++ src/main.go
@@ -2,11 +2,24 @@ package main
 
 import (
 	"log"
+	"main/logic/version"
 	"main/router"
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	log.Printf("Running LEX Version %d", version.LEX_VERSION)
+	log.Println("Checking for updates...")
+	ok, latest := version.CheckForUpdates()
+
+	if !ok {
+		log.Println("There was an error while attempting to check for updates, try again later.")
+	} else if version.LEX_VERSION < latest {
+		log.Printf("Your LEX version is outdated (version mismatch -> [gh:%d | local:%d])\r\n", latest, version.LEX_VERSION)
+	} else {
+		log.Println("You are running the latest version of LEX")
+	}
 
 	router.StartServer()
 }

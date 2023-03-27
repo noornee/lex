991e0daa23ce6018dee8ff7bd363b8ab82825685
cmd777 <83428931+cmd777@users.noreply.github.com>
2023-03-27 20:19:08 +0200

    Fix a bug where dev version would not find version Also added a flag to disable update checking

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"main/logic/version"
 	"main/router"
@@ -10,20 +11,25 @@ import (
 
 func main() {
 	if !fiber.IsChild() {
-		log.SetFlags(log.LstdFlags | log.Lshortfile)
+		checkUpdate := flag.Bool("checkupdate", true, "enables automatic update checking")
+		flag.Parse()
 
-		if okf, CVersion := version.CurrentVersion(); okf {
-			log.Printf("Running LEX Version %d", CVersion)
-			log.Println("Checking for updates...")
-			if ok, latest := version.CheckForUpdates(); !ok {
-				log.Println("There was an error while attempting to check for updates, try again later.")
-			} else if CVersion < latest {
-				log.Printf("Your LEX version is outdated (version mismatch -> [gh:%d | local:%d])\r\n", latest, CVersion)
+		if *checkUpdate {
+			if okf, CVersion := version.CurrentVersion(); okf {
+				log.Printf("Running LEX Version %d", CVersion)
+				log.Println("Checking for updates... (to disable update checking, run the application with -checkupdate=false)")
+				if ok, latest := version.CheckForUpdates(); !ok {
+					log.Println("There was an error while attempting to check for updates, try again later.")
+				} else if CVersion < latest {
+					log.Printf("Your LEX version is outdated (version mismatch -> [gh:%d | local:%d])\r\n", latest, CVersion)
+				} else {
+					log.Println("You are running the latest version of LEX")
+				}
 			} else {
-				log.Println("You are running the latest version of LEX")
+				log.Println("Failed to read local version file. (is it missing?)")
 			}
 		} else {
-			log.Println("Failed to read local version file. (is it missing?)")
+			log.Printf("update checking disabled")
 		}
 	}
 

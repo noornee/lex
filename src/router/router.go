b3d2511aa29b58da490425fcdaa85610c912f245
cmd777 <83428931+cmd777@users.noreply.github.com>
2023-04-25 00:56:17 +0200

    rc.1 for #23

--- src/router/router.go
+++ src/router/router.go
@@ -142,8 +142,21 @@ func StartServer() {
 		"toPercentage": func(input float64) string {
 			return fmt.Sprintf("%.0f", input*100)
 		},
+		"addVarToCtx": func(input ...any) map[string]any {
+			if len(input)%2 != 0 {
+				return nil
+			}
+			d := make(map[string]any)
+			for i := 0; i < len(input); i += 2 {
+				if key, ok := input[i].(string); ok {
+					d[key] = input[i+1]
+				} else {
+					return nil
+				}
+			}
+			return d
+		},
 	})
-
 	// endregion
 
 	router := fiber.New(fiber.Config{

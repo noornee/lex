537efc942934f609ea25292dcde733c5d5b244bb
cmd777 <83428931+cmd777@users.noreply.github.com>
2023-07-19 16:14:07 +0200

    check Scanln error, unzip (wip 2), 60s timeout

--- src/logic/version/updater.go
+++ src/logic/version/updater.go
@@ -1,11 +1,13 @@
 package version
 
 import (
+	"archive/zip"
 	"context"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"path/filepath"
 	"runtime"
 	"strings"
 	"time"
@@ -17,11 +19,13 @@ func StartUpdate() {
 	log.Info("A new version was detected, do you wish to install it now? [Y/n]")
 
 	inputchan := make(chan string, 1)
-	timer := time.NewTimer(10 * time.Second)
+	timer := time.NewTimer(1 * time.Minute)
 
 	go func() {
 		var input string
-		fmt.Scanln(&input)
+		if _, err := fmt.Scanln(&input); err != nil {
+			log.Errorf("Failed to scan input: %w", err)
+		}
 		inputchan <- input
 	}()
 
@@ -77,5 +81,58 @@ func UpdatePrep() {
 		return
 	}
 
-	// unzip targetfile
+	log.Info("Getting current working directory")
+	currentdir, err := os.Getwd()
+	if err != nil {
+		log.Errorf("failed to get the current working directory: %w", err)
+	}
+
+	log.Infof("Unzipping %s to %s", targetfile, currentdir)
+	if err := Unzip(targetfile, currentdir); err != nil {
+		log.Errorf("Failed to unzip file: %w", err)
+	}
+
+	log.Infof("Unzipped %s without any errors", targetfile)
+}
+
+func Unzip(src, dst string) error {
+	log.Infof("Reading zip file %s", src)
+	archive, err := zip.OpenReader(src)
+	if err != nil {
+		return fmt.Errorf("failed to read zip file: %w", err)
+	}
+	defer archive.Close()
+
+	for _, file := range archive.File {
+		log.Infof("Opening file %s", file.Name)
+		reader, err := file.Open()
+		if err != nil {
+			return fmt.Errorf("failed to open file: %w", err)
+		}
+		defer reader.Close()
+
+		destpath := filepath.Join(dst, file.Name)
+
+		if file.FileInfo().IsDir() {
+			log.Infof("Creating directory %s", destpath)
+			if err := os.Mkdir(destpath, file.Mode()); err != nil {
+				return fmt.Errorf("failed to create directory: %w", err)
+			}
+		} else {
+			log.Infof("Creating file %s", destpath)
+			destfile, err := os.OpenFile(destpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, file.Mode())
+			if err != nil {
+				return fmt.Errorf("failed to create file: %w", err)
+			}
+			defer destfile.Close()
+
+			log.Infof("Copying data from reader to %s", destfile.Name())
+			if _, err := io.Copy(destfile, reader); err != nil {
+				return fmt.Errorf("failed to copy file data: %w", err)
+			}
+			log.Infof("File %s was created without any errors", destfile.Name())
+		}
+	}
+
+	return nil
 }

6787a2fa18e285a635b32975bfb16b82c0752313
cmd777 <83428931+cmd777@users.noreply.github.com>
2023-07-20 15:05:07 +0200

    updater fix (wip2)

--- src/logic/update/updater.go
+++ src/logic/update/updater.go
@@ -1,52 +1,21 @@
 package main
 
 import (
-	"archive/zip"
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
-	"path/filepath"
 	"runtime"
-	"strings"
 	"time"
 
+	"github.com/cmd777/lex/src/logic/version"
 	"github.com/gofiber/fiber/v2/log"
 )
 
 func main() {
-	log.Info("A new version was detected, do you wish to install it now? [Y/n]")
-
-	inputchan := make(chan string, 1)
-	timer := time.NewTimer(1 * time.Minute)
-
-	go func() {
-		var input string
-		if _, err := fmt.Scanln(&input); err != nil {
-			log.Errorf("Failed to scan input: %w", err)
-		}
-		inputchan <- input
-	}()
-
-	select {
-	case in := <-inputchan:
-		if strings.HasPrefix(strings.ToLower(in), "y") {
-			log.Info("Starting update")
-			UpdatePrep()
-		} else {
-			log.Info("Canceling update")
-			if launchLEX() {
-				os.Exit(0)
-			}
-		}
-		return
-	case <-timer.C:
-		log.Warn("Timeout exceeded, not updating")
-		return
-	}
+	UpdatePrep()
 }
 
 func UpdatePrep() {
@@ -91,7 +60,7 @@ func UpdatePrep() {
 	}
 
 	log.Infof("Unzipping %s to %s", targetfile, currentdir)
-	if err := Unzip(targetfile, currentdir); err != nil {
+	if err := version.Unzip(targetfile, currentdir, "updater"); err != nil {
 		log.Errorf("Failed to unzip file: %w", err)
 	}
 
@@ -115,71 +84,7 @@ func UpdatePrep() {
 	}
 }
 
-func Unzip(src, dst string) error {
-	log.Infof("Reading zip file %s", src)
-	archive, err := zip.OpenReader(src)
-	if err != nil {
-		return fmt.Errorf("failed to read zip file: %w", err)
-	}
-	defer func() {
-		if err := archive.Close(); err != nil {
-			log.Errorf("failed to close zip file: %w", err)
-		}
-	}()
-
-	for _, file := range archive.File {
-		log.Infof("Opening file %s", file.Name)
-		reader, err := file.Open()
-		if err != nil {
-			return fmt.Errorf("failed to open file: %w", err)
-		}
-
-		destpath, err := checkInvalidPath(dst, file.Name)
-		if err != nil {
-			return err
-		}
-
-		destpath = filepath.Clean(destpath)
-
-		if file.FileInfo().IsDir() {
-			log.Infof("Creating directory %s", destpath)
-			if err := os.Mkdir(destpath, file.Mode()); err != nil {
-				return fmt.Errorf("failed to create directory: %w", err)
-			}
-		} else {
-			log.Infof("Creating file %s", destpath)
-			destfile, err := os.OpenFile(destpath, 0x00001|0x00040|0x00200, file.Mode()) // os.O_WRONLY|os.O_CREATE|os.O_TRUNC
-			if err != nil {
-				return fmt.Errorf("failed to create file: %w", err)
-			}
-
-			log.Infof("Copying %d bytes from reader to %s", file.FileInfo().Size(), destfile.Name())
-			if _, err := io.CopyN(destfile, reader, file.FileInfo().Size()); err != nil {
-				return fmt.Errorf("failed to copy file data: %w", err)
-			}
-			log.Infof("File %s was created without any errors", destfile.Name())
-
-			if err := destfile.Close(); err != nil {
-				return fmt.Errorf("failed to close created file: %w", err)
-			}
-		}
-
-		if err := reader.Close(); err != nil {
-			return fmt.Errorf("failed to close reader: %w", err)
-		}
-	}
-
-	return nil
-}
-
-func checkInvalidPath(k, v string) (string, error) {
-	destpath := filepath.Join(k, v)
-	if !strings.HasPrefix(destpath, filepath.Clean(k)) {
-		return "", errors.New("illegal filepath")
-	}
-	return destpath, nil
-}
-
+// #nosec G204
 func launchLEX() bool {
 	var cmd *exec.Cmd
 
@@ -187,27 +92,57 @@ func launchLEX() bool {
 	case "windows":
 		switch runtime.GOARCH {
 		case "amd64":
-			cmd = exec.Command("lex-amd64-windows.exe")
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Errorf("failed to get current working directory: %w", err)
+				return false
+			}
+			cmd = exec.Command(fmt.Sprintf("%s%clex-amd64-windows.exe", wd, os.PathSeparator))
 		case "386":
-			cmd = exec.Command("lex-i386-windows.exe")
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Errorf("failed to get current working directory: %w", err)
+				return false
+			}
+			cmd = exec.Command(fmt.Sprintf("%s%clex-i386-windows.exe", wd, os.PathSeparator))
 		default:
 			return false
 		}
 	case "linux":
 		switch runtime.GOARCH {
 		case "amd64":
-			cmd = exec.Command("lex-amd64-linux")
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Errorf("failed to get current working directory: %w", err)
+				return false
+			}
+			cmd = exec.Command(fmt.Sprintf("%s%clex-amd64-linux", wd, os.PathSeparator))
 		case "386":
-			cmd = exec.Command("lex-i386-linux")
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Errorf("failed to get current working directory: %w", err)
+				return false
+			}
+			cmd = exec.Command(fmt.Sprintf("%s%clex-i386-linux", wd, os.PathSeparator))
 		default:
 			return false
 		}
 	case "darwin":
 		switch runtime.GOARCH {
 		case "amd64":
-			cmd = exec.Command("lex-amd64-darwin")
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Errorf("failed to get current working directory: %w", err)
+				return false
+			}
+			cmd = exec.Command(fmt.Sprintf("%s%clex-amd64-darwin", wd, os.PathSeparator))
 		case "arm64":
-			cmd = exec.Command("lex-arm64-darwin")
+			wd, err := os.Getwd()
+			if err != nil {
+				log.Errorf("failed to get current working directory: %w", err)
+				return false
+			}
+			cmd = exec.Command(fmt.Sprintf("%s%clex-arm64-darwin", wd, os.PathSeparator))
 		default:
 			return false
 		}
--- src/logic/version/version.go
+++ src/logic/version/version.go
@@ -1,15 +1,20 @@
 package version
 
 import (
+	"archive/zip"
 	"context"
 	"embed"
+	"errors"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"runtime"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/gofiber/fiber/v2/log"
 )
@@ -25,7 +30,7 @@ const (
 	updaterPath = "https://raw.githubusercontent.com/cmd777/lex/main/src/logic/version/updater_VERSION.txt"
 )
 
-func CurrentVersion() (_ bool, _ int) {
+func currentAppVersion() (_ bool, _ int) {
 	intver, err := staticVersion.ReadFile("VERSION.txt")
 	if err != nil {
 		log.Errorf("failed to read version file: %w", err)
@@ -41,7 +46,7 @@ func CurrentVersion() (_ bool, _ int) {
 	return true, version
 }
 
-func CurrentUpdaterVersion() (_ bool, _ int) {
+func currentUpdaterVersion() (_ bool, _ int) {
 	intver, err := updaterVersion.ReadFile("updater_VERSION.txt")
 	if err != nil {
 		log.Errorf("failed to read updater version file: %w", err)
@@ -57,7 +62,7 @@ func CurrentUpdaterVersion() (_ bool, _ int) {
 	return true, version
 }
 
-func CheckForUpdates() (_ bool, _ int) {
+func checkForAppUpdates() (_ bool, _ int) {
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, versionPath, http.NoBody)
 	if err != nil {
 		log.Errorf("Failed to create New Request with Context: %w", err)
@@ -91,7 +96,7 @@ func CheckForUpdates() (_ bool, _ int) {
 	return true, version
 }
 
-func CheckForUpdaterUpdates() (_ bool, _ int) {
+func checkForUpdaterUpdates() (_ bool, _ int) {
 	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, updaterPath, http.NoBody)
 	if err != nil {
 		log.Errorf("Failed to create New Request with Context: %w", err)
@@ -126,7 +131,7 @@ func CheckForUpdaterUpdates() (_ bool, _ int) {
 }
 
 // #nosec G204
-func LaunchUpdater() bool {
+func launchUpdater() bool {
 	var cmd *exec.Cmd
 
 	switch runtime.GOOS {
@@ -198,4 +203,205 @@ func LaunchUpdater() bool {
 	}
 
 	return true
+}
+
+func CheckForUpdates() {
+	if okf, cversion := currentAppVersion(); okf {
+		log.Infof("Running LEX Version %d", cversion)
+		log.Info("Checking for updates... (to disable update checking, run the application with -checkupdate=false)")
+
+		if ok, latest := checkForAppUpdates(); !ok {
+			log.Error("There was an error while attempting to check for updates, try again later.")
+		} else if cversion < latest {
+			log.Warnf("Your LEX version is outdated (version mismatch -> [gh:%d | local:%d])", latest, cversion)
+
+			log.Info("Checking for updater updates...")
+
+			if oku, cuversion := currentUpdaterVersion(); oku {
+				log.Infof("Updater version is %d", cuversion)
+
+				if okuc, nuversion := checkForUpdaterUpdates(); !okuc {
+					log.Error("There was an error while attempting to check for updater updates, do you wish to use the old one? [Y/n]")
+					if inputHandler() {
+						if launchUpdater() {
+							os.Exit(0)
+						}
+					}
+				} else if cuversion < nuversion {
+					log.Warnf("Updater is outdated (version mismatch -> [gh:%d | local:%d])", nuversion, cuversion)
+					log.Warn("Do you wish to install the new updater now? [Y/n]")
+					UpdateUpdater()
+				} else {
+					log.Info("Updater is up-to-date, do you wish to update LEX now? [Y/n]")
+					if inputHandler() {
+						if launchUpdater() {
+							os.Exit(0)
+						}
+					}
+				}
+			}
+		} else {
+			log.Info("You are running the latest version of LEX")
+		}
+	}
+}
+
+func inputHandler() bool {
+	inputchan := make(chan string, 1)
+	timer := time.NewTimer(1 * time.Minute)
+
+	go func() {
+		var input string
+		if _, err := fmt.Scanln(&input); err != nil {
+			log.Errorf("Failed to scan input: %w", err)
+		}
+		inputchan <- input
+	}()
+
+	select {
+	case in := <-inputchan:
+		if strings.HasPrefix(strings.ToLower(in), "y") {
+			return true
+		} else {
+			return false
+		}
+	case <-timer.C:
+		log.Warn("Timeout exceeded, not updating")
+		return false
+	}
+}
+
+func Unzip(src, dst string, skipfile ...string) error {
+	log.Infof("Reading zip file %s", src)
+	archive, err := zip.OpenReader(src)
+	if err != nil {
+		return fmt.Errorf("failed to read zip file: %w", err)
+	}
+	defer func() {
+		if err := archive.Close(); err != nil {
+			log.Errorf("failed to close zip file: %w", err)
+		}
+	}()
+
+	for _, file := range archive.File {
+		if skipfile[0] != "" && strings.Contains(file.Name, skipfile[0]) {
+			continue
+		}
+
+		log.Infof("Opening file %s", file.Name)
+		reader, err := file.Open()
+		if err != nil {
+			return fmt.Errorf("failed to open file: %w", err)
+		}
+
+		destpath, err := checkInvalidPath(dst, file.Name)
+		if err != nil {
+			return err
+		}
+
+		destpath = filepath.Clean(destpath)
+
+		if file.FileInfo().IsDir() {
+			log.Infof("Creating directory %s", destpath)
+			if err := os.Mkdir(destpath, file.Mode()); err != nil {
+				return fmt.Errorf("failed to create directory: %w", err)
+			}
+		} else {
+			log.Infof("Creating file %s", destpath)
+			destfile, err := os.OpenFile(destpath, 0x00001|0x00040|0x00200, file.Mode()) // os.O_WRONLY|os.O_CREATE|os.O_TRUNC
+			if err != nil {
+				return fmt.Errorf("failed to create file: %w", err)
+			}
+
+			log.Infof("Copying %d bytes from reader to %s", file.FileInfo().Size(), destfile.Name())
+			if _, err := io.CopyN(destfile, reader, file.FileInfo().Size()); err != nil {
+				return fmt.Errorf("failed to copy file data: %w", err)
+			}
+			log.Infof("File %s was created without any errors", destfile.Name())
+
+			if err := destfile.Close(); err != nil {
+				return fmt.Errorf("failed to close created file: %w", err)
+			}
+		}
+
+		if err := reader.Close(); err != nil {
+			return fmt.Errorf("failed to close reader: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func checkInvalidPath(k, v string) (string, error) {
+	destpath := filepath.Join(k, v)
+	if !strings.HasPrefix(destpath, filepath.Clean(k)) {
+		return "", errors.New("illegal filepath")
+	}
+	return destpath, nil
+}
+
+func UpdateUpdater() {
+	targetfile := fmt.Sprintf("updater-%s.zip", runtime.GOOS)
+	targeturl := fmt.Sprintf("https://github.com/cmd777/lex/releases/download/updater/%s", targetfile)
+
+	log.Infof("Creating request to %s", targeturl)
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, targeturl, http.NoBody)
+	if err != nil {
+		log.Errorf("Failed to create New Request with Context: %w", err)
+		return
+	}
+
+	log.Info("Asking DefaultClient to do the request")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Errorf("DefaultClient failed to do the request: %w", err)
+		return
+	}
+
+	log.Infof("Reading body (%d bytes)", resp.ContentLength)
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Errorf("failed to read response body: %w", err)
+		return
+	}
+
+	if closeerr := resp.Body.Close(); closeerr != nil {
+		log.Errorf("Failed to close response body: %w", closeerr)
+	}
+
+	log.Infof("Writing %d bytes to %s", len(body), targetfile)
+	if err := os.WriteFile(targetfile, body, 0o600); err != nil {
+		log.Errorf("failed to write to file: %w", err)
+		return
+	}
+
+	log.Info("Getting current working directory")
+	currentdir, err := os.Getwd()
+	if err != nil {
+		log.Errorf("failed to get the current working directory: %w", err)
+	}
+
+	log.Infof("Unzipping %s to %s", targetfile, currentdir)
+	if err := Unzip(targetfile, currentdir); err != nil {
+		log.Errorf("Failed to unzip file: %w", err)
+	}
+
+	log.Infof("Unzipped %s without any errors", targetfile)
+
+	log.Infof("Removing %s from %s", targetfile, currentdir)
+
+	if err := os.Remove(targetfile); err != nil {
+		log.Errorf("failed to remove %s: %w", targetfile, err)
+	}
+
+	log.Info("Updater was updated successfully, starting the Updater and exiting LEX in 60 seconds.")
+
+	timer := time.NewTimer(1 * time.Minute)
+
+	for range timer.C {
+		timer.Stop()
+		if launchUpdater() {
+			os.Exit(0)
+		}
+	}
 }
